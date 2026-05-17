0cb5019b788ecd587813435cd07cf84a3ba8b154
Master_ard <master_ard@outlook.com>
2026-05-17 11:50:54 +0200

    added first launch error logging

--- main.go
+++ main.go
@@ -10,12 +10,14 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"path/filepath"
 
 	"github.com/fatih/color"
 	"golang.org/x/sys/unix"
 )
 
 func main() {
+	Firstlaunch()
 	fd := int(os.Stdin.Fd())
 
 	unix.Setpgid(0, 0)
@@ -70,5 +72,30 @@ func main() {
 		unix.IoctlSetInt(fd, unix.TIOCSPGRP, c.Process.Pid)
 		c.Wait()
 		unix.IoctlSetInt(fd, unix.TIOCSPGRP, unix.Getpgrp())
+	}
+}
+
+func Firstlaunch() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("could not get home directory")
+		return
+	}
+	marker := filepath.Join(home, ".CGTerm_init")
+
+	// if file does  !exist > first run
+	if _, err := os.Stat(marker); os.IsNotExist(err) {
+
+		fmt.Println(color.RedString("WARNING:"))
+		fmt.Println(color.RedString("Sudo command is currently bugged and may crash."))
+		fmt.Println(color.RedString("Avoid using it until fixed."))
+
+		// create marker
+		file, err := os.Create(marker)
+		if err != nil {
+			fmt.Println("could not create marker file:", err)
+			return
+		}
+		file.Close()
 	}
 }
